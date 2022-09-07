112e2fb026850ab3c06b648574d7052fec4a7830
Mastan Sayyad <101971980+MastanSayyad@users.noreply.github.com>
2022-09-07 20:33:09 +0530

    Add files via upload

---
+++ addtwonumbers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, sum;
+
+    cout << "enter number 1: ";
+    cin >> num1;
+
+    cout << "enter number 2: ";
+    cin >> num2;
+
+    sum = num1 + num2;
+
+    cout << "Addition of two numbers is: " << sum << endl;
+
+    return 0;
+}
